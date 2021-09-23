33dd9b526ceb2068d3497ea2171f92cc5b0c9314
Argon42043 <argon42043@gmail.com>
2021-09-23 09:48:37 +0200

    formated code

--- repodata/downloader.py
+++ repodata/downloader.py
@@ -5,13 +5,14 @@ import json
 import os
 import sys
 
-#downloadToDir:         path to server folder
-#manifestPath:          path to main manifest
+
+# downloadToDir:         path to server folder
+# manifestPath:          path to main manifest
 def forgeInstallerDownloader(downloadToDir, manifestPath):
 
     headers = {'User-Agent': 'forgeInstallerDownloader'}
 
-    #Read manifest.json
+    # Read manifest.json
     try:
         m = open(manifestPath, 'r')
         manifest = json.load(m)
@@ -20,25 +21,26 @@ def forgeInstallerDownloader(downloadToDir, manifestPath):
         print('manifest cant be accessed or does not exist')
         exit()
 
-    #Get forge and minecraft version from manifest
+    # Get forge and minecraft version from manifest
     try:
         print('Reading manifest')
         minecraftVersion = manifest['minecraft']['version']
         forgeVersion = manifest['minecraft']['modLoaders'][0]['id']
-        forgeVersion = forgeVersion.replace("forge-","")
+        forgeVersion = forgeVersion.replace("forge-", "")
     except:
         print('forge or minecraft version cant be read from manifest')
         exit()
-    
+
     try:
-    #download forge data
+        # download forge data
         print('Downloading Forge-' + minecraftVersion + '-' + forgeVersion)
-        forgeData = requests.get('https://maven.minecraftforge.net/net/minecraftforge/forge/' + minecraftVersion + '-' + forgeVersion + '/forge-' + minecraftVersion + '-' + forgeVersion + '-installer.jar')
+        forgeData = requests.get('https://maven.minecraftforge.net/net/minecraftforge/forge/' + minecraftVersion +
+                                 '-' + forgeVersion + '/forge-' + minecraftVersion + '-' + forgeVersion + '-installer.jar')
     except:
         print('forge version cant be downloaded')
         exit()
 
-    #save forge data to file
+    # save forge data to file
     try:
         f = open(os.path.join(downloadToDir, 'forge-Installer.jar'), 'wb')
         f.write(forgeData.content)
@@ -48,14 +50,14 @@ def forgeInstallerDownloader(downloadToDir, manifestPath):
         exit()
 
 
-#downloadToDir:         path to mod folder
-#manifestPath:          path to main manifest
-#exceptManifestPath:    path to manifest of mods that shouldn't be included
-def modDownloader(downloadToDir, manifestPath, exceptManifestPath = None):
+# downloadToDir:         path to mod folder
+# manifestPath:          path to main manifest
+# exceptManifestPath:    path to manifest of mods that shouldn't be included
+def modDownloader(downloadToDir, manifestPath, exceptManifestPath=None):
 
     headers = {'User-Agent': 'modDownloader'}
 
-    #Read manifest
+    # Read manifest
     try:
         m = open(manifestPath, 'r')
         manifest = json.load(m)
@@ -64,7 +66,7 @@ def modDownloader(downloadToDir, manifestPath, exceptManifestPath = None):
         print('manifest cant be accessed or does not exist')
         exit()
 
-    #Read exceptManifest
+    # Read exceptManifest
     if exceptManifestPath != None:
         try:
             m = open(exceptManifestPath, 'r')
@@ -73,38 +75,43 @@ def modDownloader(downloadToDir, manifestPath, exceptManifestPath = None):
         except:
             print('except-manifest cant be accessed or does not exist')
             exit()
-        #copy exceptions in list
+        # copy exceptions in list
         exceptFilesProjectIDs = []
         for projectID in exceptManifest['exceptFilesProjectIDs']:
             exceptFilesProjectIDs.append(projectID)
 
-    #Get mods from manifest
+    # Get mods from manifest
     for file in manifest['files']:
-        #get mod information
+        # get mod information
         try:
-            modInformation = requests.get('https://addons-ecs.forgesvc.net/api/v2/addon/' + str(file['projectID']) + '/file/' + str(file['fileID']) + '/', headers=headers).json()
+            modInformation = requests.get('https://addons-ecs.forgesvc.net/api/v2/addon/' + str(
+                file['projectID']) + '/file/' + str(file['fileID']) + '/', headers=headers).json()
         except:
-            print('cant get information for projectID: ' + str(file['projectID']) + ' with fileID: ' + str(file['fileID']))
+            print('cant get information for projectID: ' +
+                  str(file['projectID']) + ' with fileID: ' + str(file['fileID']))
             exit()
 
-        #check if mod should be downloaded
+        # check if mod should be downloaded
         if file['projectID'] in exceptFilesProjectIDs:
             print('Excepted ' + modInformation["fileName"])
             continue
 
-        #get mod data
+        # get mod data
         print('Downloading ' + modInformation["fileName"])
         modData = requests.get(modInformation["downloadUrl"])
         if modData.status_code != 200:
-            print('cant download ' + modInformation["fileName"] + ' from ' + modInformation["downloadUrl"] + ' because of status code ' + str(modData.status_code))
+            print('cant download ' + modInformation["fileName"] + ' from ' +
+                  modInformation["downloadUrl"] + ' because of status code ' + str(modData.status_code))
             exit()
-        
-        #save mod data
+
+        # save mod data
         try:
             if file['required'] == True:
-                f = open(os.path.join(downloadToDir, modInformation["fileName"]), 'wb')
+                f = open(os.path.join(downloadToDir,
+                         modInformation["fileName"]), 'wb')
             else:
-                f = open(os.path.join(downloadToDir, modInformation["fileName"] + '.disabled'), 'wb')
+                f = open(os.path.join(downloadToDir,
+                         modInformation["fileName"] + '.disabled'), 'wb')
 
             f.write(modData.content)
             f.close()
@@ -121,31 +128,30 @@ if __name__ == '__main__':
             downloader.py mods <mods dir> <manifest file> [excepmanifest file]')
         exit()
 
-
     args = sys.argv
 
-    #check for min args len
-    if(len(args)< 4 or len(args) > 5):
+    # check for min args len
+    if(len(args) < 4 or len(args) > 5):
         commandHelp()
 
-    #path of script
+    # path of script
     currentDir = os.getcwd()
 
-    #complete download path
+    # complete download path
     downloadDir = os.path.abspath(args[2])
     print(downloadDir)
 
-    #complete manifest path
+    # complete manifest path
     manifestFile = os.path.abspath(args[3])
-    
-    #complete excepmanifest path
+
+    # complete excepmanifest path
     if len(args) == 5:
         excepManifestFile = os.path.abspath(args[4])
-        
+
     else:
         excepManifestFile = None
 
-    #select mode
+    # select mode
     if args[1] == "forge":
         forgeInstallerDownloader(downloadDir, manifestFile)
 
@@ -153,4 +159,5 @@ if __name__ == '__main__':
         modDownloader(downloadDir, manifestFile, excepManifestFile)
 
     else:
+
         commandHelp
--- repodata/zip.py
+++ repodata/zip.py
@@ -3,7 +3,7 @@ from zipfile import ZipFile
 import json
 import os
 
-#read manifest
+# read manifest
 try:
     m = open('repodata/manifest.json', 'r')
     manifest = json.load(m)
@@ -14,17 +14,18 @@ except:
 
 zipfileName = manifest['name'] + '-' + manifest['version'] + '.zip'
 
-#create zipfile
+# create zipfile
 zip = ZipFile(zipfileName, 'w')
-#add manifest and modlist
+
+# add manifest and modlist
 zip.write('repodata/manifest.json', 'manifest.json')
 zip.write('repodata/modlist.html', 'modlist.html')
 
-#add overrides folder recursive
+# add overrides folder recursive
 for dirname, subdirs, files in os.walk('overrides'):
     zip.write(dirname)
     for filename in files:
         zip.write(os.path.join(dirname, filename))
 
 zip.close()
-print('Created '+ zipfileName)
+print('Created ' + zipfileName)
